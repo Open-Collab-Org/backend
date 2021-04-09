dd8b36580881749f5861ca0eb6a9a02c8c951d1b
Lucas Antunes <lucasba8@gmail.com>
2021-04-09 14:38:16 -0300

    feat: revamp the routing DI system
    The routing DI system now supports:
    - Easy to add request-specific providers
    - Providers by interface

--- routes/routes.go
+++ routes/routes.go
@@ -42,6 +42,13 @@ func SetupRoutes(router *mux.Router, providers []interface{}) {
 	}
 }
 
+type HandlerMetadata struct {
+	handler     interface{}
+	handlerType reflect.Type
+	handlerName string
+	handlerPath string
+}
+
 // This method is used to create gin route handlers with a few conveniences.
 // It returns a gin route handler that calls the handler you supplied with a
 // database reference and automatic error handling. All you have to do is
@@ -60,63 +67,14 @@ func createRouteHandler(handler interface{}, providers []interface{}) func(http.
 
 	checkReturnTypes(handlerType, handlerName)
 
+	handlerMetadata := HandlerMetadata{
+		handler,
+		handlerType,
+		handlerName,
+		handlerPath,
+	}
+
 	return func(writer http.ResponseWriter, request *http.Request) {
-		// Populate two arrays with the underlying types of each value in providers
-		// and whether that value is a pointer or not.
-		providerTypes := make([]reflect.Type, len(providers))
-		providerTypesPtrs := make([]bool, len(providers))
-		for i, provider := range providers {
-			providerType, isPtr := getType(reflect.TypeOf(provider))
-
-			providerTypes[i] = providerType
-			providerTypesPtrs[i] = isPtr
-		}
-
-		// Create an array of arguments that will be used to call handler.
-		// These arguments are found by matching the handler parameter types
-		// against the provider types, when there is a match the respective provider
-		// will be used as argument for the respective parameter.
-		handlerParamCount := handlerType.NumIn()
-		handlerArgs := make([]reflect.Value, handlerParamCount)
-		for i := 0; i < handlerParamCount; i++ {
-			paramType, isPtr := getType(handlerType.In(i))
-
-			if paramType.Implements(reflect.TypeOf((*http.ResponseWriter)(nil)).Elem()) {
-				if isPtr {
-					panic(fmt.Sprintf("Handler %s: Cannot pass *http.ResponseWriter (pointer). Use http.Request (value) instead.", handlerName))
-				} else {
-					handlerArgs[i] = reflect.ValueOf(writer)
-				}
-
-				continue
-			}
-
-			if paramType == reflect.TypeOf(http.Request{}) {
-				if isPtr {
-					handlerArgs[i] = reflect.ValueOf(request)
-				} else {
-					panic(fmt.Sprintf("Handler %s: Cannot pass http.Request (value). Use *http.Request (pointer) instead.", handlerName))
-				}
-
-				continue
-			}
-
-			for providerIdx, provider := range providers {
-				// Check if param and provider are of the same type
-				// (and whether or not they're both pointers)
-				if isPtr == providerTypesPtrs[providerIdx] && paramType == providerTypes[providerIdx] {
-					handlerArgs[i] = reflect.ValueOf(provider)
-
-					break
-				}
-			}
-
-			if !handlerArgs[i].IsValid() {
-				panic(fmt.Sprintf("The provider %s was not found for parameter %d of handler %s", paramType.Name(), i, handlerPath))
-			}
-		}
-
-		ctx := context.Background()
 		requestId, err := uuid.NewV4()
 		if err != nil {
 			log.WithError(err).Error("Failed to generate a request id.")
@@ -125,25 +83,128 @@ func createRouteHandler(handler interface{}, providers []interface{}) func(http.
 			return
 		}
 
+		ctx := context.Background()
 		logger := log.WithFields(log.Fields{
 			"requestId": requestId,
 		})
-		log.NewContext(ctx, logger)
+		ctx = log.NewContext(ctx, logger)
 
-		// Call the handler
-		returnValues := reflect.ValueOf(handler).Call(handlerArgs)
-		var routeErr error = nil
-
-		// We know the returned value is error because we checked it earlier
-		if !returnValues[0].IsNil() {
-			routeErr = returnValues[0].Interface().(error)
+		rsProviders := []interface{}{
+			ctx,
+			&writer,
+			request,
 		}
 
-		// Handle the error returned by the handler, if any
-		if routeErr != nil {
-			handleRouteError(writer, ctx, routeErr)
+		err = callHandler(handlerMetadata, providers, rsProviders)
+		if err != nil {
+			handleRouteError(writer, ctx, err)
 		}
 	}
+}
+
+// Calls a handler
+// TODO: document this properly
+// Note: if you want to make a provider accessible as an interface, you
+// have to pass a pointer to the interface as provider. Example:
+// Assume myProvider implements MyIface
+// If we want the handler to receive a MyIface parameter,
+// rsProviders has to be []interface{}{ &MyIface(myProvider) }.
+func callHandler(
+	handlerMetadata HandlerMetadata,
+	providers []interface{},
+	rsProviders []interface{},
+) error {
+	// Populate two arrays with the underlying types of each value in providers
+	// and whether that value is a pointer or not.
+	providerTypes := make([]reflect.Type, len(providers))
+	providerTypesPtrs := make([]bool, len(providers))
+	for i, provider := range providers {
+		providerType, isPtr := getType(reflect.TypeOf(provider))
+
+		providerTypes[i] = providerType
+		providerTypesPtrs[i] = isPtr
+	}
+
+	rsProviderTypes := make([]reflect.Type, len(rsProviders))
+	rsProviderTypesPtrs := make([]bool, len(rsProviders))
+	for i, rsProvider := range rsProviders {
+		rsProviderType, isPtr := getType(reflect.TypeOf(rsProvider))
+
+		rsProviderTypes[i] = rsProviderType
+		rsProviderTypesPtrs[i] = isPtr
+	}
+
+	// Create an array of arguments that will be used to call handler.
+	// These arguments are found by matching the handler parameter types
+	// against the provider types, when there is a match the respective provider
+	// will be used as argument for the respective parameter.
+	handlerParamCount := handlerMetadata.handlerType.NumIn()
+	handlerArgs := make([]reflect.Value, handlerParamCount)
+paramLoop:
+	for i := 0; i < handlerParamCount; i++ {
+		paramType, isPtr := getType(handlerMetadata.handlerType.In(i))
+
+		for rsProviderIdx, rsProvider := range rsProviders {
+			rsProviderType := rsProviderTypes[rsProviderIdx]
+			rsProviderIsPtr := rsProviderTypesPtrs[rsProviderIdx]
+
+			rsProviderIsIface := rsProviderType.Kind() == reflect.Interface
+
+			if rsProviderIsIface && paramType == rsProviderType {
+				// If we're here, rsProviderType is of kind interface. This tells us that rsProvider
+				// is a pointer for sure, since the only way to get an interface type is through
+				// the Elem() of the pointer-to-interface type.
+				// This means that if the handler is expecting an interface argument, we have to
+				// dereference the value of rsProvider (with reflect.Indirect) before passing it to the handler.
+				// If the handler is expecting a pointer to the interface, we can just pass the value of rsProvider,
+				// since it already is a pointer to the interface.
+				if isPtr {
+					handlerArgs[i] = reflect.ValueOf(rsProvider)
+				} else {
+					handlerArgs[i] = reflect.Indirect(reflect.ValueOf(rsProvider))
+				}
+
+				continue
+			}
+
+			if rsProviderType == paramType && rsProviderIsPtr == isPtr {
+				handlerArgs[i] = reflect.ValueOf(rsProvider)
+
+				continue paramLoop
+			}
+		}
+
+		for providerIdx, provider := range providers {
+			// Check if param and provider are of the same type
+			// (and whether or not they're both pointers)
+			if isPtr == providerTypesPtrs[providerIdx] && paramType == providerTypes[providerIdx] {
+				handlerArgs[i] = reflect.ValueOf(provider)
+
+				continue paramLoop
+			}
+		}
+
+		if !handlerArgs[i].IsValid() {
+			panic(
+				fmt.Sprintf("The provider %s was not found for parameter %d of handler %s",
+					paramType.Name(),
+					i,
+					handlerMetadata.handlerPath,
+				),
+			)
+		}
+	}
+
+	// Call the handler
+	returnValues := reflect.ValueOf(handlerMetadata.handler).Call(handlerArgs)
+	routeErr := error(nil)
+
+	// We know the returned value is error because we checked it earlier
+	if !returnValues[0].IsNil() {
+		routeErr = returnValues[0].Interface().(error)
+	}
+
+	return routeErr
 }
 
 func getType(t reflect.Type) (reflect.Type, bool) {
@@ -181,13 +242,19 @@ func handleRouteError(writer http.ResponseWriter, ctx context.Context, routeErr 
 	details := map[string]interface{}{}
 
 	switch e := routeErr.(type) {
+	default:
+		writer.WriteHeader(500)
+
 	case *json.SyntaxError:
 		code = "json-syntax-error"
 		details["offset"] = fmt.Sprintf("%d", e.Offset)
+		writer.WriteHeader(400)
 
 	case *json.UnmarshalTypeError:
 		code = "json-type-error"
 		details["field"] = e.Field
+		writer.WriteHeader(400)
+
 	}
 
 	err := httpUtils.WriteJson(writer, ctx, map[string]interface{}{
@@ -198,8 +265,6 @@ func handleRouteError(writer http.ResponseWriter, ctx context.Context, routeErr 
 		logger.WithError(err).Error("Failed to write error response")
 		writer.WriteHeader(500)
 	}
-
-	writer.WriteHeader(400)
 }
 
 func logRoute(route *mux.Route, _ *mux.Router, _ []*mux.Route) error {
