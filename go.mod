55ad71f7ad27801254085ccf3918369b3c025c50
Lucas Antunes <lucasba8@gmail.com>
2021-04-10 11:48:49 -0300

    use godi package for dependency injection

--- go.mod
+++ go.mod
@@ -3,31 +3,21 @@ module github.com/open-collaboration/server
 go 1.16
 
 require (
+	github.com/ItsaMeTuni/godi v0.0.0-20210410034142-393252e8d661
 	github.com/apex/log v1.9.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/go-gormigrate/gormigrate/v2 v2.0.0
-	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gofrs/uuid v3.2.0+incompatible
-	github.com/golang/protobuf v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/ugorji/go v1.2.4 // indirect
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 	gorm.io/driver/postgres v1.0.0
 	gorm.io/driver/sqlite v1.1.4 // indirect
--- routes/routes.go
+++ routes/routes.go
@@ -3,15 +3,14 @@ package routes
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/ItsaMeTuni/godi"
 	"github.com/apex/log"
 	"github.com/gofrs/uuid"
 	"github.com/gorilla/mux"
 	"github.com/open-collaboration/server/httpUtils"
 	"net/http"
-	"reflect"
-	"runtime"
-	"strings"
 )
 
 type RouteResponse struct {
@@ -21,15 +20,6 @@ type RouteResponse struct {
 
 // Sets up all routes in the application.
 func SetupRoutes(router *mux.Router, providers []interface{}) {
-	for _, provider := range providers {
-		providerType := reflect.TypeOf(provider)
-		if providerType.Kind() != reflect.Ptr {
-			log.
-				WithField("provider", providerType.Name()).
-				Warn("By-value provider detected, this is allowed but not recommended. Consider passing the provider as a pointer.")
-		}
-	}
-
 	router.HandleFunc("/users", createRouteHandler(RouteRegisterUser, providers)).Methods("POST")
 	router.HandleFunc("/login", createRouteHandler(RouteAuthenticateUser, providers)).Methods("POST")
 	router.HandleFunc("/projects", createRouteHandler(RouteCreateProject, providers)).Methods("POST")
@@ -42,36 +32,15 @@ func SetupRoutes(router *mux.Router, providers []interface{}) {
 	}
 }
 
-type HandlerMetadata struct {
-	handler     interface{}
-	handlerType reflect.Type
-	handlerName string
-	handlerPath string
-}
-
 // This method is used to create gin route handlers with a few conveniences.
 // It returns a gin route handler that calls the handler you supplied with a
 // database reference and automatic error handling. All you have to do is
 // supply a routeHandler and the rest will be taken care of for you.
 func createRouteHandler(handler interface{}, providers []interface{}) func(http.ResponseWriter, *http.Request) {
 
-	handlerType := reflect.TypeOf(handler)
-	if handlerType.Kind() != reflect.Func {
-		panic("Supplied handler is not a func!")
-	}
-
-	handlerPath := runtime.FuncForPC(reflect.ValueOf(handler).Pointer()).Name()
-
-	handlerPathParts := strings.Split(handlerPath, ".")
-	handlerName := handlerPathParts[len(handlerPathParts)-1]
-
-	checkReturnTypes(handlerType, handlerName)
-
-	handlerMetadata := HandlerMetadata{
-		handler,
-		handlerType,
-		handlerName,
-		handlerPath,
+	err := godi.AssertFn(handler, []interface{}{errors.New("")})
+	if err != nil {
+		panic(err)
 	}
 
 	return func(writer http.ResponseWriter, request *http.Request) {
@@ -89,149 +58,26 @@ func createRouteHandler(handler interface{}, providers []interface{}) func(http.
 		})
 		ctx = log.NewContext(ctx, logger)
 
-		rsProviders := []interface{}{
-			ctx,
-			&writer,
-			request,
-		}
+		reqProviders := append(
+			godi.Providers{
+				ctx,
+				writer,
+				request,
+			},
+			providers...,
+		)
 
-		err = callHandler(handlerMetadata, providers, rsProviders)
+		returnValues, err := godi.Inject(handler, reqProviders)
 		if err != nil {
-			handleRouteError(writer, ctx, err)
-		}
-	}
-}
-
-// Calls a handler
-// TODO: document this properly
-// Note: if you want to make a provider accessible as an interface, you
-// have to pass a pointer to the interface as provider. Example:
-// Assume myProvider implements MyIface
-// If we want the handler to receive a MyIface parameter,
-// rsProviders has to be []interface{}{ &MyIface(myProvider) }.
-func callHandler(
-	handlerMetadata HandlerMetadata,
-	providers []interface{},
-	rsProviders []interface{},
-) error {
-	// Populate two arrays with the underlying types of each value in providers
-	// and whether that value is a pointer or not.
-	providerTypes := make([]reflect.Type, len(providers))
-	providerTypesPtrs := make([]bool, len(providers))
-	for i, provider := range providers {
-		providerType, isPtr := getType(reflect.TypeOf(provider))
-
-		providerTypes[i] = providerType
-		providerTypesPtrs[i] = isPtr
-	}
-
-	rsProviderTypes := make([]reflect.Type, len(rsProviders))
-	rsProviderTypesPtrs := make([]bool, len(rsProviders))
-	for i, rsProvider := range rsProviders {
-		rsProviderType, isPtr := getType(reflect.TypeOf(rsProvider))
-
-		rsProviderTypes[i] = rsProviderType
-		rsProviderTypesPtrs[i] = isPtr
-	}
-
-	// Create an array of arguments that will be used to call handler.
-	// These arguments are found by matching the handler parameter types
-	// against the provider types, when there is a match the respective provider
-	// will be used as argument for the respective parameter.
-	handlerParamCount := handlerMetadata.handlerType.NumIn()
-	handlerArgs := make([]reflect.Value, handlerParamCount)
-paramLoop:
-	for i := 0; i < handlerParamCount; i++ {
-		paramType, isPtr := getType(handlerMetadata.handlerType.In(i))
-
-		for rsProviderIdx, rsProvider := range rsProviders {
-			rsProviderType := rsProviderTypes[rsProviderIdx]
-			rsProviderIsPtr := rsProviderTypesPtrs[rsProviderIdx]
-
-			rsProviderIsIface := rsProviderType.Kind() == reflect.Interface
-
-			if rsProviderIsIface && paramType == rsProviderType {
-				// If we're here, rsProviderType is of kind interface. This tells us that rsProvider
-				// is a pointer for sure, since the only way to get an interface type is through
-				// the Elem() of the pointer-to-interface type.
-				// This means that if the handler is expecting an interface argument, we have to
-				// dereference the value of rsProvider (with reflect.Indirect) before passing it to the handler.
-				// If the handler is expecting a pointer to the interface, we can just pass the value of rsProvider,
-				// since it already is a pointer to the interface.
-				if isPtr {
-					handlerArgs[i] = reflect.ValueOf(rsProvider)
-				} else {
-					handlerArgs[i] = reflect.Indirect(reflect.ValueOf(rsProvider))
-				}
-
-				continue
-			}
-
-			if rsProviderType == paramType && rsProviderIsPtr == isPtr {
-				handlerArgs[i] = reflect.ValueOf(rsProvider)
-
-				continue paramLoop
-			}
+			logger.WithError(err).Error("Failed to call handler and inject dependencies on it.")
+			return
 		}
 
-		for providerIdx, provider := range providers {
-			// Check if param and provider are of the same type
-			// (and whether or not they're both pointers)
-			if isPtr == providerTypesPtrs[providerIdx] && paramType == providerTypes[providerIdx] {
-				handlerArgs[i] = reflect.ValueOf(provider)
-
-				continue paramLoop
-			}
+		// Handlers should return an error (or nil), as we asserted above
+		if !returnValues[0].IsNil() {
+			e := returnValues[0].Interface().(error)
+			handleRouteError(writer, ctx, e)
 		}
-
-		if !handlerArgs[i].IsValid() {
-			panic(
-				fmt.Sprintf("The provider %s was not found for parameter %d of handler %s",
-					paramType.Name(),
-					i,
-					handlerMetadata.handlerPath,
-				),
-			)
-		}
-	}
-
-	// Call the handler
-	returnValues := reflect.ValueOf(handlerMetadata.handler).Call(handlerArgs)
-	routeErr := error(nil)
-
-	// We know the returned value is error because we checked it earlier
-	if !returnValues[0].IsNil() {
-		routeErr = returnValues[0].Interface().(error)
-	}
-
-	return routeErr
-}
-
-func getType(t reflect.Type) (reflect.Type, bool) {
-	paramType := t
-	isPtr := paramType.Kind() == reflect.Ptr
-	if isPtr {
-		paramType = paramType.Elem()
-
-		if paramType.Kind() == reflect.Ptr {
-			panic("Double pointers are not supported by the Dependency Injection system.")
-		}
-	}
-
-	return paramType, isPtr
-}
-
-func checkReturnTypes(handlerType reflect.Type, handlerName string) {
-	msg := fmt.Sprintf("%s: Handlers should have an error return type", handlerName)
-
-	returnValueCount := handlerType.NumOut()
-	if returnValueCount != 1 {
-		panic(msg)
-	}
-
-	firstRetVal := handlerType.Out(0)
-	if firstRetVal != reflect.TypeOf((*error)(nil)).Elem() {
-		panic(msg)
 	}
 }
 
